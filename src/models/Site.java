eea28decf8b9097a69b745e870c3c458accab269
AimilianosKass <44274366+AimiKass@users.noreply.github.com>
2019-03-17 23:24:27 +0200

    model updated , gitignore added,

--- src/models/Site.java
+++ src/models/Site.java
@@ -5,4 +5,33 @@ public class Site
     String url;
     String name;
 
+    public Site(String url, String name)
+    {
+        this.url = url;
+        this.name = name;
+    }
+
+    @Override
+
+    public String toString()
+    {
+        // TODO: 3/17/2019 make it look beautiful :)
+        return (name+"       ("+url+")");
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
